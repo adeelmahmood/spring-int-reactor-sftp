3fe5f208c4c16646a0e6acc3823a1092295a2659
Adeel Qureshi <adeelq@Adeels-MacBook-Air.local>
2015-05-05 17:46:18 -0500

    testing reactor dispatcher with threadcount

--- src/main/java/demo/Application.java
+++ src/main/java/demo/Application.java
@@ -2,10 +2,12 @@ package demo;
 
 import java.util.UUID;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ImportResource;
 import org.springframework.util.IdGenerator;
 
 import reactor.Environment;
@@ -15,16 +17,24 @@ import reactor.spring.context.config.EnableReactor;
 
 @SpringBootApplication
 @EnableReactor
-@ImportResource("classpath:sftp.xml")
-public class Application {
+// @ImportResource("classpath:sftp.xml")
+public class Application implements CommandLineRunner {
 
 	static {
 		Environment.initializeIfEmpty().assignErrorJournal();
 	}
 
+	@Value("${reactor.thread.count:6}")
+	private int threadCount;
+
+	@Value("${reactor.backlog:256}")
+	private int backlog;
+
 	@Bean
 	public EventBus eventBus() {
-		return EventBus.config().env(Environment.get()).dispatcher(Environment.SHARED).get();
+		return EventBus.config().env(Environment.get())
+				.dispatcher(Environment.newDispatcher(backlog, threadCount))
+				.get();
 	}
 
 	@Bean
@@ -39,5 +49,14 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Autowired
+	private EventBus eventBus;
+
+	@Override
+	public void run(String... args) throws Exception {
+		System.out.println("Checking reactor configuration");
+		System.out.println("backlog = " + eventBus.getDispatcher().backlogSize());
 	}
 }
